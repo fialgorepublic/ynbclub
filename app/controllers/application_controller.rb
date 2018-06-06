cce6dc7b0f8e7c2928a098bb21adc70ca611e4a7
Muhammad.Ali <alisheikh4021@gmail.com>
2018-06-06 15:01:53 +0500

    Implemented take snapshot scenario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_variables
+  before_action :allow_iframe_requests
+  before_action :allow_user_request
 
   def set_variables
     @shopify_domain = "test-saint.myshopify.com"
@@ -22,6 +24,19 @@ class ApplicationController < ActionController::Base
 
   def clear_shopify_session
     ShopifyAPI::Base.clear_session
+  end
+
+  private
+
+  def allow_user_request
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+
+  def allow_iframe_requests
+    response.headers.delete('X-Frame-Options')
   end
 
 end
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,15 @@ module SaintLBeauApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.action_dispatch.default_headers['P3P'] = 'CP="Not used"'
+    config.action_dispatch.default_headers.delete('X-Frame-Options')
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
+    config.action_dispatch.default_headers = {
+        'X-XSS-Protection' => '0;'
+    }
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
