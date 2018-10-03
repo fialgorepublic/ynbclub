bb9001dfd2061dae6b48136f20cf2445e52f2bf2
Muhammad.Ali <alisheikh4021@gmail.com>
2018-10-03 21:03:32 +0500

    Signed-off-by: Muhammad.Ali <alisheikh4021@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,12 +65,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-      :address              => "smtp.gmail.com",
-      :port                 => 587,
-      :domain               => "gmail.com",
-      :user_name            => "info.saintlbeau@gmail.com",
-      :password             => "info@12345",
-      :authentication       => :plain,
+      :user_name => 'imranarshad',
+      :password => 'Imran123#',
+      :domain => 'sendgrid.com',
+      :address => 'smtp.sendgrid.net',
+      :port => 587,
+      :authentication => :plain,
       :enable_starttls_auto => true
   }
   # Ignore bad email addresses and do not raise email delivery errors.
