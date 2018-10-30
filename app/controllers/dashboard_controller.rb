636cba6a9a0ccc12ff4b8b546e5287c63a5ec878
Muhammad.Ali <alisheikh4021@gmail.com>
2018-10-30 14:54:16 +0500

    Signed-off-by: Muhammad.Ali <alisheikh4021@gmail.com>

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -17,20 +17,16 @@ class DashboardController < ApplicationController
   end
 
   def update_user_role
-    if params[:role_id].present?
-      current_user.update_attributes(role_id: params[:role_id])
-      flash[:notice] = "Successfully updated role"
-      if current_user.role.name == "Buyer"
-        buyer = "true"
-      else
-        referral = Devise.friendly_token
-        current_user.update_attributes(referral: referral)
-        buyer = "false"
-      end
-      render json: {success: true, :buyer => buyer}
+    current_user.update_attributes(role_id: params[:role_id])
+    flash[:notice] = "Successfully updated role"
+    if current_user.role.name == "Buyer"
+      buyer = "true"
     else
-      render json: {success: false}
+      referral = Devise.friendly_token
+      current_user.update_attributes(referral: referral)
+      buyer = "false"
     end
+    render json: {success: true, :buyer => buyer}
   end
 
   def change_profile_picture
