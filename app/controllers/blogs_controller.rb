87eb640a02bf307b130ac4bafd80c1cf53ec5db2
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-02 20:37:31 +0500

    Show all the blogs to user.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -7,7 +7,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = current_user.blogs.order('created_at ASC').paginate(page: params[:page], per_page: 10)
+    @blogs = Blog.all.order('created_at ASC').paginate(page: params[:page], per_page: 10)
   end
 
   # GET /blogs/1
