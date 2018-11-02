b642e2465b070c6f26563b0b99cbb38ceec70be9
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-02 18:21:22 +0500

    Fix email change and validation issue.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   include ApplicationHelper
   before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  load_and_authorize_resource except: [:add_user_info]
+  load_and_authorize_resource except: [:add_user_info, :update_email]
   require 'csv'
   require 'roo'
 
