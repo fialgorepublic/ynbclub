117edf2789faae0185772d1c1f86e31dc57c1083
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-08 17:56:17 +0500

    Fix my orders code breakdown.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,8 @@ class OrdersController < ApplicationController
 
   def my_orders
     initiate_shopify_session
-    @orders = get_customer_id.present? ? ShopifyAPI::Order.find(:all, :params => {:status => "any",customer_id: customer_id ,:limit => 250}) : []
+    customer_id = get_customer_id
+    @orders = customer_id.present? ? ShopifyAPI::Order.find(:all, :params => {:status => "any",customer_id: customer_id ,:limit => 250}) : []
     clear_shopify_session
   end
 
