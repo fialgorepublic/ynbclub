cc4a5af73f120e490b1372d763c204b9fd122722
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-14 15:12:12 +0500

    Save share_ur_id to points table when someone share this.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   before_action :authenticate_user!, except: [:blog_detail]
-  before_action :set_blog, except: [:blog_like_unlike, :index, :new, :create, :show, :change_featured_state]
+  before_action :set_blog, except: [:blog_like_unlike, :index, :new, :create, :show, :change_featured_state, :share_blog]
   require 'time_ago_in_words'
   require 'will_paginate'
   include ApplicationHelper
@@ -117,7 +117,7 @@ class BlogsController < ApplicationController
 
   def share_blog
     share_url = ShareUrl.create(user_id: current_user.id, blog_id: params[:id], url_type: params[:value])
-    insert_points(current_user.id, 3)
+    insert_points(current_user.id, 3, "", share_url.id)
     render json: {success: true}
   end
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -55,9 +55,9 @@ module ApplicationHelper
     Setting.first ? Setting.first.min_payment : 0
   end
 
-  def insert_points(user_id, point_id, invitee="")
+  def insert_points(user_id, point_id, invitee="", share_url_id=nil)
     point_type = PointType.find_by_id(point_id)
-    Point.create(user_id: user_id, point_type_id: point_type.id, point_value: point_type.point, invitee: invitee)
+    Point.create(user_id: user_id, point_type_id: point_type.id, point_value: point_type.point, invitee: invitee, share_url_id: share_url_id)
   end
 
   def get_point(id)
