9a9ce7842c406711e7f698ec0fe28d1084c885a0
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-15 12:53:06 +0500

    Fix buyer login issue.

--- app/models/share_url.rb
+++ app/models/share_url.rb
@@ -2,5 +2,5 @@ class ShareUrl < ApplicationRecord
   belongs_to :user
   belongs_to :blog, optional: true
 
-  delegate :title, to: :blog, prefix: true
+  delegate :title, to: :blog, prefix: true, allow_nil: true
 end
