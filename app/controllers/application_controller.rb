15972fb41d31082ea8a4bd21d70d4abfcd6ab08e
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-22 14:20:37 +0500

    Fix blogs not found issue.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,8 +45,9 @@ class ApplicationController < ActionController::Base
 
   def blog_not_found
     return unless user_signed_in?
+    return if params[:blog_not_found].blank?
 
     return redirect_to buyerDashboard_path(blog_not_found: true) if controller_name == "home" && current_user.is_buyer?
-    redirect_to dashboard_path(blog_not_found: true) if controller_name == "home"
+    redirect_to dashboard_path(blog_not_found: true) if controller_name == "home
   end
 end
