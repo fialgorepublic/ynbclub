8adb9d27fbaad61b7a1dc67b0b02256403246ac1
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-29 15:03:19 +0500

    Fix blog sharing issue.

--- app/models/share_url.rb
+++ app/models/share_url.rb
@@ -7,6 +7,7 @@ class ShareUrl < ApplicationRecord
   before_create :set_shared_website
 
   def set_shared_website
+    return if url.blank?
     return self.url_type = "facebook" if url.include?("facebook")
     self.url_type = "twitter" if url.include?("twitter")
   end
