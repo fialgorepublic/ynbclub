d4586a92ed495dd4fe3f7cad4d1e91e64e4a25ea
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-29 18:43:45 +0500

    Add controller and views to update share_with_friends values.

--- app/controllers/share_with_friends_controller.rb
+++ app/controllers/share_with_friends_controller.rb
@@ -1,2 +1,24 @@
 class ShareWithFriendsController < ApplicationController
+  before_action :set_share_with_friend
+
+  def edit
+  end
+
+  def update
+    if @share_with_friend.update(share_with_friends_params)
+      redirect_to share_with_friends_path, notice: "Changes are updated successfully."
+    else
+      flash[:alert] = @share_with_friend.errors.full_messages
+      render :edit
+    end
+  end
+
+  private
+    def share_with_friends_params
+      params.require(:share_with_friend).permit!
+    end
+
+    def set_share_with_friend
+      @share_with_friend = ShareWithFriend.first
+    end
 end
