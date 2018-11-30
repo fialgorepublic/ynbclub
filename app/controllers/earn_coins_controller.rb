bb7c74b9133bd20fdbc55de19767cd607fab35b6
Talha Junaid <talha.junaid@algorepublic.com>
2018-11-30 17:57:15 +0500

    Add earn coins controller and its routes.

---
+++ app/controllers/earn_coins_controller.rb
@@ -0,0 +1,2 @@
+class EarnCoinsController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -90,7 +90,8 @@ Rails.application.routes.draw do
     resources :cookies
   end
 
-  resource :share_with_friends, only: [:edit, :update]
+  resource  :share_with_friends, only: [:edit, :update]
+  resources :earn_coins, only: [:edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
