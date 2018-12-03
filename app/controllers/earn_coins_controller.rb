a5ce7c862ab295714f1e372109525c7493306b4d
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-03 15:00:05 +0500

    Add edit & update actions in earn_coins controller.

--- app/controllers/earn_coins_controller.rb
+++ app/controllers/earn_coins_controller.rb
@@ -1,2 +1,19 @@
 class EarnCoinsController < ApplicationController
+
+  def edit
+    @earn_coin.point_types
+  end
+
+  def update
+    if @earn_coin.update(earn_coin_params)
+      redirect_to page_design_path, notice: "Changes saved successfully."
+    else
+      render :edit
+    end
+  end
+
+  private
+    def earn_coin_params
+      params.require(:earn_coin).permit(:main_text, :how_earn_text, :how_spend_text, :earn_way, :coins, :price, point_types_attributes: [:id, :name, :point, :_destroy])
+    end
 end
