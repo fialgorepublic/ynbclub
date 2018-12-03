505680638247d1deadfc8dc1c27d2919ccca741a
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-03 14:59:07 +0500

    Add destroy option in model.

--- app/models/earn_coin.rb
+++ app/models/earn_coin.rb
@@ -1,5 +1,5 @@
 class EarnCoin < ApplicationRecord
   has_many :point_types
 
-  accepts_nested_attributes_for :point_types
+  accepts_nested_attributes_for :point_types, reject_if: :all_blank, allow_destroy: true
 end
