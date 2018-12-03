c8228d115cce1e89f111f44dd50605a8ce9a31cc
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-03 15:09:13 +0500

    Fix ID conflict.

--- app/models/point_type.rb
+++ app/models/point_type.rb
@@ -1,4 +1,11 @@
 class PointType < ApplicationRecord
   belongs_to :earn_coin
   has_many :points
+
+  before_create :set_id
+
+  def set_id
+    max_id = PointType.maximum(:id)
+    self.id = max_id.present? ? max_id + 1 : 1
+  end
 end
