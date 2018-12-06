318aa8801b36dda817f0de577e6e4d9e8e2137b6
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-06 12:08:25 +0500

    Restrict the use to uplaod image files only and add validations.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def image_url user
     user.avatar.url == "/images/original/missing.png" ? "/assets/user-img-2.png" : user.avatar.url
   end
+
+  def profile_pic_name user
+    user.avatar.url == "/images/original/missing.png" ? "No file Chosen" : user.avatar_file_name
+  end
 end
