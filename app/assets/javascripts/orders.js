63d8b76e0d607ff781c7ea157b55451dca91578d
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-07 15:05:49 +0500

    Setup datatable for referral-sales.

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
   $('#my-orders').dataTable( {
     "pagingType": "full_numbers"
   });
+
+  $('#referral-sales').dataTable( {
+    "pagingType": "full_numbers"
+  });
 })
