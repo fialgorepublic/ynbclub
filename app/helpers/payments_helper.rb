d7f12f9f13da49a8273e003a765e9cd16ce746fb
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-10 19:00:43 +0500

    Show selected values.

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,2 +1,13 @@
 module PaymentsHelper
+  def selected_since_date params
+    params[:search].present? ? params[:search][:start_date] : nil
+  end
+
+  def selected_o_date params
+    params[:search].present? ? params[:search][:to_date] : nil
+  end
+
+  def selected_partner params
+    params[:search].present? ? params[:search][:partner] : nil
+  end
 end
