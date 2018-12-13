e17346a01d6c003d87639ad5319f4fe33fcf2d34
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-13 16:36:26 +0500

    Add paperclip setting to page model.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,11 @@
 class Page < ApplicationRecord
+  has_attached_file :image,
+                    :default_url => "/images/:style/missing.png",
+                    :storage => :s3,
+                    :url => 's3_domain_url',
+                    :s3_host_alias => 'saintalgorepublic.s3-website-us-east-1.amazonaws.com',
+                    :s3_credentials => File.join(Rails.root, 'config', 's3.yml'),
+                    :path => "/files/:style/:id_:filename",
+                    styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
