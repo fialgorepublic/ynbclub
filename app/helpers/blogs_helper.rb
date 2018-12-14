f580648855f02dbcb6b3a9df2e7b8be43981a589
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-14 19:03:07 +0500

    Fixes.

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,8 @@ module BlogsHelper
   def file_name blog
     blog.avatar.url == "/images/original/missing.png" ? "No file Chosen" : blog.avatar_file_name
   end
+
+  def blog_attributes blog
+    { id: blog.id, title: blog.title, file_name: blog.avatar_file_name, description: blog.description.html_safe }.to_json
+  end
 end
