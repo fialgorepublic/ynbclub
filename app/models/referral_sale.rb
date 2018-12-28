b9797074be6d54fa4f83313bd76291a437571ea2
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-28 12:08:26 +0500

    Refactor the code.

--- app/models/referral_sale.rb
+++ app/models/referral_sale.rb
@@ -5,11 +5,7 @@ class ReferralSale < ApplicationRecord
   after_save :set_ambassador_value
 
   private
-  def set_ambassador_value
-    if (self.is_approved == true)
-      user = self.user
-      total_income = user.total_income
-      user.update_attributes(total_income: total_income.to_f + self.price.to_f)
+    def set_ambassador_value
+      user.update_attributes(total_income: user.total_income.to_f + price.to_f) if is_approved?
     end
-  end
 end
