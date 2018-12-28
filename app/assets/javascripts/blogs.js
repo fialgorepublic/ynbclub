664e9547d31683dd95f52beebd4e5cb9ac8182a5
Talha Junaid <talha.junaid@algorepublic.com>
2018-12-28 18:32:18 +0500

    Add ajax to load more blogs.

--- app/assets/javascripts/blogs.js
+++ app/assets/javascripts/blogs.js
@@ -17,4 +17,28 @@ $(document).ready(function() {
       }
     })
   })
+
+  $("#load-more").click(function() {
+    page = $('#page').val();
+
+    $('.loader').show();
+
+    $.ajax({
+      url: `/blogs?page=${page}`,
+      type: 'GET',
+      dataType: 'json',
+      success: function(data) {
+        $('.loader').hide();
+        $('#blogs').append(data.attachmentPartial);
+        $('#page').val(data.next_page)
+        if(data.next_page == data.total_pages) {
+          $('#load-more').hide();
+        }
+      },
+      error: function(data) {
+        $('.loader').hide()
+        alert('Something Wentwrong!')
+      }
+    })
+  })
 });
