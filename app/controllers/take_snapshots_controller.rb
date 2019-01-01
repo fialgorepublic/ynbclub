fc6feb50df4a9d7eacc7452f66f747f9db0e202d
Talha Junaid <talha.junaid@algorepublic.com>
2019-01-01 15:54:20 +0500

    Add edit & update actions in take_snapshots controller.

--- app/controllers/take_snapshots_controller.rb
+++ app/controllers/take_snapshots_controller.rb
@@ -1,2 +1,17 @@
 class TakeSnapshotsController < ApplicationController
+  def edit
+  end
+
+  def update
+    if @snapshot.update(snapshot_params)
+      redirect_to take_snapshot_path, notice: "Succeffully updated."
+    else
+      render :edit
+    end
+  end
+
+  private
+    def snapshot_params
+      params.require(:snapshot).permit(:step1_avatar, :step2_avatar, :step3_avatar, :step4_avatar, :step1_text, :step2_text, :step3_text, :step4_text)
+    end
 end
