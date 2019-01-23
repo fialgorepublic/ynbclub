fe3d759fbee6e89bfd11177eaadc84e8efb6ab3b
Talha Junaid <talha.junaid@algorepublic.com>
2019-01-23 16:04:21 +0500

    Remove unused files.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,67 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_product, only: [:show, :edit, :update, :destroy]
   require 'time_ago_in_words'
-
-  # GET /products
-  # GET /products.json
-  def index
-    @products = Product.all
-  end
-
-  # GET /products/1
-  # GET /products/1.json
-  def show
-  end
-
-  # GET /products/new
-  def new
-    @product = Product.new
-  end
-
-  # GET /products/1/edit
-  def edit
-  end
-
-  # POST /products
-  # POST /products.json
-  def create
-    @product = Product.new(product_params)
-
-    respond_to do |format|
-      if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { render :new }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /products/1
-  # PATCH/PUT /products/1.json
-  def update
-    respond_to do |format|
-      if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
-        format.json { render :show, status: :ok, location: @product }
-      else
-        format.html { render :edit }
-        format.json { render json: @product.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /products/1
-  # DELETE /products/1.json
-  def destroy
-    @product.destroy
-    respond_to do |format|
-      format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   def get_products_from_shopify
     @blog = Blog.find_by_id(params[:id])
@@ -76,16 +15,5 @@ class ProductsController < ApplicationController
     @selected_products = ShopifyAPI::Product.where(ids: params[:ids])
     clear_shopify_session
     render partial: 'blogs/selected_products'
-  end
-
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_product
-    @product = Product.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def product_params
-    params.require(:product).permit(:title, :product_id, :price, :currency, :avatar)
   end
 end
