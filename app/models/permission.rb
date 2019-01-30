1ca19383cd63f186c424dae9c91459573a7177de
Talha Junaid <talha.junaid@algorepublic.com>
2019-01-30 18:39:16 +0500

    Fix page desing option issue.

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -2,6 +2,6 @@ class Permission < ApplicationRecord
   belongs_to :user
 
   CONTROLLER_NAMES = { 'approve_sales': 'referral_sales', 'manage_rewards': 'point_types', 'configurations': 'settings', 'ban': 'users', 'deduct_points': 'users', 'brand_ambassadors': 'users',
-                      'ambassadors': 'users', 'index': 'payments', 'categories': 'categories',  'page_desgin': 'dashboard'
+                      'ambassadors': 'users', 'index': 'payments', 'categories': 'categories',  'page_design': 'dashboard'
                     }
 end
