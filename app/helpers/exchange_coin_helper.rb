8ea450733d4b8d15e712e17ffc44abaee14839c3
Talha Junaid <talha.junaid@algorepublic.com>
2019-02-04 14:06:12 +0500

    Make amount field dynamic.

--- app/helpers/exchange_coin_helper.rb
+++ app/helpers/exchange_coin_helper.rb
@@ -1,6 +1,6 @@
 module ExchangeCoinHelper
   def format_amount_to_vnd amount
-    formatted_value = number_to_currency 100000, precision: 0, delimiter: '.'
+    formatted_value = number_to_currency amount, precision: 0, delimiter: '.'
     "#{formatted_value.remove('$')}₫"
   end
 end
