2b9a229226e1029a49778dbe053c262a660f2c6a
Talha Junaid <talha.junaid@algorepublic.com>
2019-02-04 14:06:36 +0500

    Add function to filter price value from special characters.

--- app/helpers/exchange_coin_helper.rb
+++ app/helpers/exchange_coin_helper.rb
@@ -1,6 +1,10 @@
 module ExchangeCoinHelper
-  def format_amount_to_vnd amount
+  def format_amount_to_vnd(amount)
     formatted_value = number_to_currency amount, precision: 0, delimiter: '.'
     "#{formatted_value.remove('$')}₫"
+  end
+
+  def filter_the_amount(price)
+    price.remove('₫').split(',').join('')
   end
 end
