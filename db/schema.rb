5af2da5501f745b3fa8431c45ba7eed8aeaf07d7
Talha Junaid <talha.junaid@algorepublic.com>
2019-02-27 19:29:34 +0500

    Add model state and its migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190125130254) do
+ActiveRecord::Schema.define(version: 20190227142844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -286,6 +286,12 @@ ActiveRecord::Schema.define(version: 20190125130254) do
     t.string "step2_text"
     t.string "step3_text"
     t.string "step4_text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "states", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
