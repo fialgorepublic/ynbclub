acdb6589eafad92af6bcdc75f10ac6464732fc06
Talha Junaid <talha.junaid@algorepublic.com>
2019-03-06 16:03:52 +0500

    Add association in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
+  belongs_to :city,     optional: true
+  belongs_to :district, optional: true
+  belongs_to :province, optional: true
+  belongs_to :ward,     optional: true
+
   has_many :items
 
   accepts_nested_attributes_for :items
