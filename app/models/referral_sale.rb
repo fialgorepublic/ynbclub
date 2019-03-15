d3a8dde4fd4f93a2a0d5cac9e9032931f7c32696
Talha Junaid <talha.junaid@algorepublic.com>
2019-03-15 14:48:27 +0500

    Fix undefined method issue.

--- app/models/referral_sale.rb
+++ app/models/referral_sale.rb
@@ -26,7 +26,7 @@ class ReferralSale < ApplicationRecord
   def order_ghtk_status
     order =  Order.find_by(order_id: self.order_id)
     return 'GHTK Order Not Found' if order.blank?
-    order.status.present? ? order.status : 'Status not updated yet.'
+    order.status.present? ? order.ghtk_status : 'Status not updated yet.'
   end
 
   private
