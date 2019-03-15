20dfe0ece8b7497704ad941d7c05bb38fc4e5e5c
Talha Junaid <talha.junaid@algorepublic.com>
2019-03-15 13:19:10 +0500

    Change where condition.

--- app/jobs/update_order_status_job.rb
+++ app/jobs/update_order_status_job.rb
@@ -3,7 +3,7 @@ class UpdateOrderStatusJob < ApplicationJob
 
   def perform(*args)
     orders = Order.where.not(ghtk_label: nil)
-                  .where.not(ghtk_status: ['Delivered / Uncontrolled', 'Delivered (COD has finished delivering goods)'])
+                  .where.not(ghtk_status: 'Delivered (COD has finished delivering goods)')
 
     UpdateOrderStatusService.new(orders).call if orders.present?
   end
