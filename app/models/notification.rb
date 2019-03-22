d6e235f31291b99bcd8ca83f36e047cba221041f
Talha Junaid <talha.junaid@algorepublic.com>
2019-03-22 13:03:13 +0500

    Add delegate.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :source, polymorphic: true
   belongs_to :target, class_name: 'User', optional: true
+
+  delegate :old_income, :new_income, :order_no, :ghtk_status, to: :source, prefix: true
 end
