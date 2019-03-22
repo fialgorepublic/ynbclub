4e7872207c5e48e7246c1ef3a94c1cf5e4eda01b
Talha Junaid <talha.junaid@algorepublic.com>
2019-03-22 11:46:36 +0500

    Update association.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,4 @@
 class Notification < ApplicationRecord
   belongs_to :source, polymorphic: true
-  belongs_to :target, class_name: 'User'
+  belongs_to :target, class_name: 'User', optional: true
 end
