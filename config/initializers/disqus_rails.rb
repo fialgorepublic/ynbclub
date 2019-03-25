89d88fde0350743d105720eb09fd1445b960a62d
Talha Junaid <talha.junaid@algorepublic.com>
2019-03-25 15:58:33 +0500

    Remove

--- config/initializers/disqus_rails.rb
+++
@@ -1,6 +0,0 @@
-DisqusRails.setup do |config|
-  config::SHORT_NAME = "saintlbeau-affiliate"
-  config::SECRET_KEY = "rYhwzsEzLFQOZv7irh78vW3RXd9MPH7wpM0QwLpbdEyjjNSoURJ8HhlugZ09qsoT" #leave blank if not used
-  config::PUBLIC_KEY = "dTNboyeYy8FrKbKFGXM0vGIKcdc3YWq7a4bJXMZdlei9h5JZSVbwh2W0rlSicbG8" #leave blank if not used
-  config::ACCESS_TOKEN = "151874a7b27c4fbba08c24ef8eec2ebd" #you got it, right? ;-)
-end
