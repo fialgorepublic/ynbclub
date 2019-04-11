9a2aacd7993d3e5af1f75fe9f8d037a871ced605
Talha Junaid <talha.junaid@algorepublic.com>
2019-04-11 12:01:37 +0500

    Add column image_url

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190410072033) do
+ActiveRecord::Schema.define(version: 20190411070059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20190410072033) do
     t.boolean "is_published", default: false
     t.boolean "buyer_show", default: false
     t.bigint "user_id"
+    t.string "image_url"
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
