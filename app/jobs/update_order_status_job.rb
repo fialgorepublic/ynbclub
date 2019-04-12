e88275bab49d05165d851591eb9af0e2385e4ee6
Talha Junaid <talha.junaid@algorepublic.com>
2019-04-12 19:09:34 +0500

    Send email before and after order status job.

--- app/jobs/update_order_status_job.rb
+++ app/jobs/update_order_status_job.rb
@@ -2,9 +2,17 @@ class UpdateOrderStatusJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    orders = Order.where.not(ghtk_label: nil)
-                  .where.not(ghtk_status: 'Controled')
+    OrderJobMailer.start_job_email.deliver_now
+    begin
+      orders = Order.where.not(ghtk_label: nil)
+                    .where.not(ghtk_status: 'Controled')
+      UpdateOrderStatusService.new(orders).call if orders.present?
 
-    UpdateOrderStatusService.new(orders).call if orders.present?
+      message = "Orders job is completed successfully."
+    rescue => ex
+      message = "Order status job failed because of following issue: #{ex.message}"
+    end
+
+    OrderJobMailer.end_job_email(message).deliver_now
   end
 end
