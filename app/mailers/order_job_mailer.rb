02876a3ecf62a1efb7db30f68df5efac645e9dc6
Talha Junaid <talha.junaid@algorepublic.com>
2019-04-12 19:50:52 +0500

    Add lucas email to job.

--- app/mailers/order_job_mailer.rb
+++ app/mailers/order_job_mailer.rb
@@ -1,5 +1,5 @@
 class OrderJobMailer < ApplicationMailer
-  default to: 'talha.junaid@algorepublic.com'
+  default to: ['talha.junaid@algorepublic.com', 'Lucas@saintlbeau.com']
 
   def start_job_email
     mail(subject: "GHTK Order sweeper notification")
