659267dbf33fcf43c2625e21e350eb25276625e9
Talha Junaid <talha.junaid@algorepublic.com>
2019-04-16 18:44:24 +0500

    Fix image issues for user.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,10 @@
 module UsersHelper
   def image_url user
-    user.avatar.url == "/images/original/missing.png" ? "/assets/user-img-2.png" : user.avatar.url
+    user.avatar.attached? ? user.avatar : "/assets/user-img-2.png"
   end
 
   def profile_pic_name user
-    user.avatar.url == "/images/original/missing.png" ? "No file Chosen" : user.avatar_file_name
+    user.avatar.attached? ? user.avatar.filename : "No file Chosen"
   end
 
   def show_if_already_searched search_text
