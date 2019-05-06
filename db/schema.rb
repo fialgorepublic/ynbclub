c846ce693ef122ff9ab00a63ecff3315326a64f6
Talha Junaid <talha.junaid@algorepublic.com>
2019-05-06 15:59:00 +0500

    Add column slug in blog's table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_122721) do
+ActiveRecord::Schema.define(version: 2019_05_03_145643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 2019_05_01_122721) do
     t.boolean "buyer_show", default: false
     t.bigint "user_id"
     t.string "image_url"
+    t.string "slug"
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
