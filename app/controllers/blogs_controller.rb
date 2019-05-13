f19f6d3df3606f2b9f76ae50d0134d49a6573f7e
Talha Junaid <talha.junaid@algorepublic.com>
2019-05-13 12:34:05 +0500

    Fix blog rerouting issue when creating or updating the category.

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -162,6 +162,7 @@ class BlogsController < ApplicationController
       category = Category.new(category_params)
       @success = category.save
     end
+    @url = params[:url]
 
     respond_to do |format|
       format.js
