9499c960f97cdf9aa841675b4a6f4ed87d7c063b
Talha Junaid <talha.junaid@algorepublic.com>
2019-05-16 16:48:24 +0500

    Add url column to products.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_03_145643) do
+ActiveRecord::Schema.define(version: 2019_05_16_114751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -306,6 +306,7 @@ ActiveRecord::Schema.define(version: 2019_05_03_145643) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.string "url"
   end
 
   create_table "profiles", force: :cascade do |t|
