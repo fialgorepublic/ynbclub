bbdd7b25c70b310c2bf42cc839feda4d36422df9
Talha Junaid <talha.junaid@algorepublic.com>
2019-06-20 18:41:54 +0500

    Use local storage instead of amazon.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # `config/secrets.yml.key`.
   config.read_encrypted_secrets = true
 
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
