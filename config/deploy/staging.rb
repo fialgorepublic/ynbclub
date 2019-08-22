5e764271acd2eecaa9da186fe2209d0d28aa6ddd
Talha Junaid <talha.junaid@algorepublic.com>
2019-08-22 20:48:44 +0500

    Change puma.sock name.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,5 +3,3 @@ set :deploy_to,    "/home/deploy/#{fetch(:application)}"
 set :stage,        :staging
 set :rails_env,    :staging
 set :branch,       :staging
-
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
