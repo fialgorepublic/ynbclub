e1d07fa2fceabe904e21f1f7a572fdecc406a2dc
Talha Junaid <talha.junaid@algorepublic.com>
2019-08-22 20:48:44 +0500

    Change puma.sock name.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,6 +16,7 @@ set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/deploy/#{fetch(:application)}"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
