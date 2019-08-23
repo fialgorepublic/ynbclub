995a3efcc93f0d0093dc0876dcb5a099edb08d36
Talha Junaid <talha.junaid@algorepublic.com>
2019-08-23 14:29:44 +0500

    Move puma configuration to staging.

--- config/deploy.rb
+++ config/deploy.rb
@@ -16,11 +16,6 @@ set :deploy_via,      :remote_cache
 
 #Puma settings
 set :puma_rackup, -> { File.join(current_path, 'config.ru') }
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/affiliate.sock"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,3 +3,9 @@ set :deploy_to,    "/home/deploy/#{fetch(:application)}"
 set :stage,        :staging
 set :rails_env,    :staging
 set :branch,       :staging
+
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind,       "unix:///home/deploy/#{fetch(:application)}/shared/tmp/sockets/affiliate.sock"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
