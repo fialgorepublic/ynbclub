51b9604f50e8f5e4fcc44d2e15e9f2cb7a283866
Talha Junaid <talha.junaid@algorepublic.com>
2019-08-23 14:16:42 +0500

    Minor changes in puma settings.

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,16 +13,20 @@ set :puma_workers,    0
 set :pty,             true
 set :use_sudo,        false
 set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/deploy/#{fetch(:application)}"
-set :puma_bind,       "unix:///home/deploy/#{fetch(:application)}/shared/tmp/sockets/#{fetch(:application)}-puma.sock"
+
+#Puma settings
+set :puma_rackup, -> { File.join(current_path, 'config.ru') }
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/affiliate.sock"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
+set :puma_threads, [0, 16]
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
 
 ## Defaults:
 # set :scm,           :git
