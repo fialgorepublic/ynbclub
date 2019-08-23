ea48e9ad809277245561132d32ba9a1e0f8372b1
Talha Junaid <talha.junaid@algorepublic.com>
2019-08-23 11:50:36 +0500

    Add task for seeds and change puma_bind path.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,9 +14,9 @@ set :pty,             true
 set :use_sudo,        false
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/deploy/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
@@ -77,6 +77,15 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       invoke!('puma:restart')
+    end
+  end
+
+  desc "reload the database with seed data"
+  task :seed do
+    on roles(:all) do
+      within current_path do
+        execute :bundle, :exec, 'rails', 'db:seed', "RAILS_ENV=#{rails_env}"
+      end
     end
   end
 
