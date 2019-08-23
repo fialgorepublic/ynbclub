d853cb3c3c784189fcce1908bd968e455358655c
Talha Junaid <talha.junaid@algorepublic.com>
2019-08-23 18:48:47 +0500

    Set puma properties in production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,10 @@ set :deploy_to,   "/home/deploy/#{fetch(:application)}"
 set :stage,        :production
 set :rails_env,    :production
 set :branch,      :master
+
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind,       "unix:///home/deploy/#{fetch(:application)}/shared/tmp/sockets/affiliate.sock"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
