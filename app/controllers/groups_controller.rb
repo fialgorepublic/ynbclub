487d45f151a261c47fd31f17dfebabbb0c136d77
Talha Junaid <talha.junaid@algorepublic.com>
2019-10-21 18:15:43 +0500

    Update index and group controller.

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,23 +1,8 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
-  # GET /groups
-  # GET /groups.json
   def index
-    blogs = \
-        if current_user.present?
-          current_user.filtered_blogs(params[:sort], params[:category])
-        else
-          Blog.eager_load_objects.all_published_blogs(params[:sort], params[:category])
-        end
-    @blogs = blogs.paginate(page: params[:page], per_page: 10)
-    @next_page = @blogs.next_page
-    if request.xhr?
-      with_format :html do
-        @html_content = render_to_string partial: 'all_blogs'
-      end
-      render json: { attachmentPartial: @html_content, success: true, next_page: @next_page, total_pages: @blogs.total_pages, current_page: @blogs.current_page }
-    end
+    @groups = Group.all
   end
 
   # GET /groups/1
@@ -41,9 +26,11 @@ class GroupsController < ApplicationController
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to @group, notice: 'Group was successfully created.' }
+        flash[:success] =  'Group was successfully created.' 
+        format.html { redirect_to @group }
         format.json { render :show, status: :created, location: @group }
       else
+        flash[:alert] = @group.errors.full_messages.to_sentence
         format.html { render :new }
         format.json { render json: @group.errors, status: :unprocessable_entity }
       end
@@ -82,6 +69,6 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:name, :description)
+      params.require(:group).permit(:name, :description, :logo)
     end
 end
