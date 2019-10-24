e237cb1fa47cc0589100542f59388a3c33aec89b
Talha Junaid <talha.junaid@algorepublic.com>
2019-10-24 17:36:57 +0500

    Add model group_categories.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_082637) do
+ActiveRecord::Schema.define(version: 2019_10_24_123527) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,6 +156,12 @@ ActiveRecord::Schema.define(version: 2019_10_24_082637) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_exchange_histories_on_user_id"
+  end
+
+  create_table "group_categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "groups", force: :cascade do |t|
