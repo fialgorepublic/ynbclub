02a1c2dae1e7aacb739dd6da3634235985edb3d1
Talha Junaid <talha.junaid@algorepublic.com>
2019-10-24 13:29:45 +0500

    Use friendly Id in groups.

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,11 +1,11 @@
 module GroupsHelper
   def groups_page_title
-    return "Edit Group" if action_name == 'edit' && controller_name == 'groups'
+    return "Edit Group" if action_name == 'edit'
     'Create New Group'
   end
 
   def group_button_text
-    return "EDIT" if action_name == 'edit' && controller_name == 'groups'
+    return "Update" if action_name == 'edit'
     'CREATE'
   end
 
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,14 @@
 class Group < ApplicationRecord
+  extend FriendlyId
+  friendly_id do |config|
+    config.base = :name
+    config.use :slugged
+    config.use Module.new {
+      def normalize_friendly_id(text)
+        text.to_slug.normalize! :transliterations => [:vietnamese]
+      end
+    }
+  end
   has_one_attached :logo
 
   validates :name, :description, presence: true
