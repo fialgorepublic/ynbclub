d102856f5c094e25bd5c82ae9698fb33fe40efdb
Talha Junaid <talha.junaid@algorepublic.com>
2019-10-24 13:29:14 +0500

    Add slug column to groups table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_25_083543) do
+ActiveRecord::Schema.define(version: 2019_10_24_082637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -163,6 +163,8 @@ ActiveRecord::Schema.define(version: 2019_09_25_083543) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_groups_on_slug", unique: true
   end
 
   create_table "items", force: :cascade do |t|
