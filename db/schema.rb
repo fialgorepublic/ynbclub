98cbdc8cec6b0f5998ca01337ea90406f3a1fd7d
Talha Junaid <talha.junaid@algorepublic.com>
2019-11-01 15:00:54 +0500

    Add column to store the groups count.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_31_143624) do
+ActiveRecord::Schema.define(version: 2019_11_01_095759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -469,6 +469,7 @@ ActiveRecord::Schema.define(version: 2019_10_31_143624) do
     t.string "discount_code"
     t.integer "share_link_count", default: 0
     t.boolean "moderator"
+    t.integer "groups_count", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
