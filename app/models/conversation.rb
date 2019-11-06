3a2b7c88c038c295f5118e8e4c642532d7632c39
Talha Junaid <talha.junaid@algorepublic.com>
2019-11-06 18:57:34 +0500

    Add association for replies in conversation.

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,6 @@
 class Conversation < ApplicationRecord
   belongs_to :group, counter_cache: true
   belongs_to :user,  counter_cache: true
+
+  has_many   :replies, class_name: 'Conversation',  foreign_key: 'parent_id'
 end
