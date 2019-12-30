cbd404f4218957cad20795d45dcfa37ee09565e3
fahad ishfaq <fahad.ishfaq@algorepublic.com>
2019-12-30 16:34:56 +0500

    fixed issue in youtube upload in conversation.

---
+++ app/assets/javascripts/medium-editor-insert-plugin.js
@@ -0,0 +1,6 @@
+//= require handlebars/handlebars.runtime.min
+//= require jquery-sortable/jquery-sortable-min
+//= require blueimp-file-upload/jquery.ui.widget
+//= require blueimp-file-upload/jquery.iframe-transport
+//= require blueimp-file-upload/jquery.fileupload
+//= require medium-editor-insert-plugin/medium-editor-insert-plugin
