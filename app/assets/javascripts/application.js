547610231586cbdd00cd8485cbdf4a1df72d81dd
fahad ishfaq <fahad.ishfaq@algorepublic.com>
2020-03-05 18:44:37 +0500

    minor fix.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,7 +23,7 @@
 //= require apprise.min.js
 //= require select2.js
 //= require cocoon
-//= require order
+//= require orders
 //= require js.cookies.min.js
 //= require emoji/dist/emojionearea.js
 
