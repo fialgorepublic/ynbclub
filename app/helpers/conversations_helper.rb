264781bf3236fca8eb0820ea540da094f80cb104
abalgorepublic <abubakar.saddique@algorepublic.com>
2020-03-13 15:36:26 +0500

    add blog

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -52,12 +52,12 @@ module ConversationsHelper
   def like_dislike_link(conversation)
     if current_user.blank?
       link_to 'javascript:void(0);', data: { toggle: 'modal', target: '#signIn' } do
-        "<i class='fa fa-heart fs-20 green-color'></i>".html_safe
+        "<i class='fa fa-heart fs-20 blue-color'></i>".html_safe
       end
     else
       conversation_like_exists = conversation.conversation_like_exists?(current_user.id)
       link_to like_dislike_path(conversation_like_exists, conversation.id), remote: true, class: 'like-dislike-convo click', data: { conversation_id: conversation.id } do
-        "<i class='fa fa-heart fs-20 #{conversation_like_exists ? 'red-color' : 'green-color'}'></i>".html_safe
+        "<i class='fa fa-heart fs-20 #{conversation_like_exists ? 'red-color' : 'blue-color'}'></i>".html_safe
       end
     end
   end
