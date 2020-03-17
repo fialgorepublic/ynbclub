08e30ad3de8795856e57127a130216bc514b738c
fahad ishfaq <fahad.ishfaq@algorepublic.com>
2020-03-17 16:30:29 +0500

    added follow button on blog modal.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -40,7 +40,7 @@ module UsersHelper
   end
 
   def follow_unfollow_link(user)
-    return if user == current_user
+    # return if user == current_user
     if current_user.blank?
       link_to 'Follow', 'javascript:void(0);', data: { toggle: 'modal', target: '#signIn' }, class: 'btn profile-tag-btn-ad btn-default'
     else
