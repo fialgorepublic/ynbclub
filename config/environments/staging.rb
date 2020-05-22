0da183582fa18b9184a5fb4c8dc8743efffb2c9b
Talha Junaid <talha.junaid@square63.org>
2020-05-22 18:00:17 +0500

    Add production & staging url for cable.

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -45,8 +45,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://ambassador-staging.saintlbeau.com/cable'
+  config.action_cable.allowed_request_origins = [ 'https://ambassador-staging.saintlbeau.com', 'http://ambassador-staging.saintlbeau.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
