54aafc0b8ba06cf946312bc460f2f53c395a4c37
fahad ishfaq <fahadishfaq18@gmail.com>
2020-06-25 21:08:10 +0500

    change brand name in order sms.

--- lib/esms.rb
+++ lib/esms.rb
@@ -6,11 +6,12 @@ class Esms
     API_URL = 'http://rest.esms.vn/MainService.svc/json/SendMultipleMessage_V4_get'
     API_KEY = Rails.application.credentials.esms[:api_key]
     SECRET_KEY = Rails.application.credentials.esms[:secret_key]
-    SMS_TYPE = 8
+    SMS_TYPE = 2
+    Brand_Name = "SAINTLBEAU"
 
     def send_sms(to:, content:)
       begin
-        response = HTTParty.get(API_URL+"?Phone=#{to}&Content=#{content}&ApiKey=#{API_KEY}&SecretKey=#{SECRET_KEY}&SmsType=#{SMS_TYPE}")
+        response = HTTParty.get(API_URL+"?Phone=#{to}&Content=#{content}&ApiKey=#{API_KEY}&SecretKey=#{SECRET_KEY}&Brandname=#{Brand_Name}&SmsType=#{SMS_TYPE}")
         code_result = response["CodeResult"]
         message = code_result == "100" ? "Message sent to #{to} successfully" : response["ErrorMessage"]
         [code_result, message]
